701a2fe665b70e40889eadef4f1c7739e4515732
MAX3 <max-3@users.noreply.github.com>
2016-10-19 07:28:43 +0300

    go to work

--- js/ui.js
+++ js/ui.js
@@ -170,7 +170,7 @@ $(document).on("click", "#description b", function () {
 
 
 function uiShowScheme(xml, name) { //выводить SVG, пока для тестов
-  $("#show_SVG").attr("href", "//yzorrykodelie.ru/узор-спицами-№" + name);
+  $("#show_SVG").attr("href", "http://yzorrykodelie.ru/узор-спицами-№" + name);
   var html = $(xml)["0"].documentElement.outerHTML;
   $("#show_SVG").html(html);
 }
