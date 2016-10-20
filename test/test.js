c6b037e95015085df50ba0a13266408cd4f08386
max-work <max-3@users.noreply.github.com>
2016-10-20 16:44:33 +0300

    обработка символов начинающихся с цифры

---
+++ test/test.js
@@ -0,0 +1,16 @@
+$.when.apply($, deferreds).done(function () { //дождатся окончания ajax
+  //  console.info(PRICE("лицевая"));
+  //  console.info(RUS(1, "накид"));
+  console.log(RUS(1, "2 вм лиц"));
+  console.log(RUS(1, "3 вм"));
+  console.log(RUS(4, "2 вм лиц"));
+  console.log(RUS(4, "3 вм"));
+
+
+  //  console.log(RUS(25, "накsыд"));
+  //  console.log(BD);
+  // console.log(PRICE("нак и п снять"));
+  // console.log(PRICE("изнаночная")+PRICE("изнаночная")+PRICE("изнаночная"));
+  // console.log(PRICE("накид"));
+
+});
