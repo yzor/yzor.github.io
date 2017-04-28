9d0491948c863dcb541f758d2c9baa338e4f6a8a
MAX 3 <MAX 3>
2017-04-28 15:45:46 +0300

    fix

--- beta/js/ux.js
+++ beta/js/ux.js
@@ -9,8 +9,23 @@
 
 //ЗАКЛАДКИ
 
+$("#description .box")
+  .mouseup(function () {
+    //    $(this).append("<span style='color:#f00;'>Mouse up.</span>");
+    alert(1);
+  })
+  .mousedown(function () {
+    alert(2);
+    //    $(this).append("<span style='color:#00f;'>Mouse down.</span>");
+  });
+$(document).on('mousedown', '.box', function (e) {
+//  alert($(e.target).attr('class'))
+//  alert("lskfj");
+});
+
 //закладка при клике на элемент
-$(document).on("click", "#description .box", function () {
+//$(document).on("click", "#description .box", function () {
+$(document).on("mousedown", "#description .box", function () {
   if ($(this).hasClass("mark")) {
     $(this).removeClass("mark");
   } else {
@@ -44,7 +59,7 @@ $(document).on({ //наведение на номер ряда"1-й ряд:"...
 }, "#description b"); //pass the element as an argument to .on
 
 
-//выделение рапорта в текущем редуприкликена звезду
+//выделение рапорта в текущем ряду при клике на звезду
 $(document).on("click", ".star, .star2", function () {
   $(this).closest("p").find(".rapo").toggleClass("mark-rap");
 });
