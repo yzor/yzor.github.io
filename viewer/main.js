5bb386467d5c75e3c15eea3c6d0a5c91dee17cff
MAX3 <max-3@users.noreply.github.com>
2017-05-13 22:07:56 +0300

    viewer

--- viewer/main.js
+++ viewer/main.js
@@ -1,14 +1,35 @@
 /* jshint -W117 */
 /* jshint -W098 */
-var N = 23;
+var N = 33;
 // $("#t1").val("//lorempixel.com/70/350/sports/947/");
 // $("#t3").val("/");
 
+
 $(function () {
-  // Обработчик для .ready()
-  $("#t1").val("https://yzorrykodelie.ru/узор-спицами-№");
-  $("#t2").val(N);
-   next();
+  //убираем анимацию при загрузке
+  setTimeout(function () {
+    $(".fast").removeClass("fast");
+  }, 500); // время в мс
+
+
+
+  //если в url есть значения после #, то используем их, иначе 33 схема
+  var urlHash = location.hash;
+  if (urlHash) {
+    urlHash = urlHash.slice(1).split("⚡");
+    $("#t1").val(urlHash[0]);
+    N = urlHash[1];
+    $("#t3").val(urlHash[2]);
+    //N=urlHash.replace(/\D+/g,"")
+    //  console.error(urlHash.replace(/\D+/g,""));
+  } else {
+    $("#t1").val("https://yzorrykodelie.ru/узор-спицами-№");
+    $("#t2").val(N);
+  }
+
+  next1();
+
+
 
 
   // $('.txt').on('input keyup', function(e) {
@@ -18,25 +39,31 @@ $(function () {
   });
 
   $("#before").mousedown(function () {
-    if (window.event.ctrlKey) {
+    if (window.event.shiftKey) {
+      N = N - 100;
+    } else if (window.event.ctrlKey) {
       //ctrl was held down during the click
-      N = N - 9;
+      N = N - 10;
+    } else {
+      N--;
     }
-
-    N--;
     next2();
   });
   $("#after").mousedown(function () {
-    if (window.event.ctrlKey) {
+    if (window.event.shiftKey) {
+      N = N + 100;
+    } else if (window.event.ctrlKey) {
       //ctrl was held down during the click
-      N = N + 9;
+      N = N + 10;
+    } else {
+      N++;
     }
-    N++;
     next();
   });
   $("#go").mousedown(function () {
 
-    //    next();  $("iframe").contents().find("body").html()
+    next();
+    //    $("iframe").contents().find("body").html()
   });
 
 
@@ -44,7 +71,12 @@ $(function () {
 
 
 
+  function next() {
+    next2();
+  }
+
   function next1() {
+    document.title = "№" + N;
     $("#num").text(N);
     $("#num").removeClass("act");
     setTimeout(function () {
@@ -52,15 +84,16 @@ $(function () {
     }, 0); // время в мс
     var b = $("#t1").val();
     var a = $("#t3").val();
+    location.hash = b + '⚡' + N + '⚡' + a;
     $("#ribbon").text(N);
     $("#t2").val(N);
     $('iframe').attr('src', b + N + a);
+    console.info("№ " + N);
+
   }
 
-
-
-  function next() {next2();}
   function next2() {
+    document.title = "№" + N;
     $("#num").text(N);
     $("#num").removeClass("act");
     setTimeout(function () {
@@ -68,15 +101,16 @@ $(function () {
     }, 0); // время в мс
     var b = $("#t1").val();
     var a = $("#t3").val();
+    location.hash = b + '⚡' + N + '⚡' + a;
     $("#ribbon").text(N);
     $("#t2").val(N);
-    //    $('iframe').attr('src', b + N + a);
     $('iframe').addClass("old");
     $("#conteiner").append('<iframe src="' + b + N + a + '"></iframe>');
+    console.info("№ " + N);
     setTimeout(function () {
       $(".old").remove();
     }, 1000); // время в мс
-
   }
 
 });
+//#mark5:4
